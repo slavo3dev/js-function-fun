9dc6c579ee4c38cf7a155609604ad95f76c816c5
manjabogicevic <slavo@mimicom24.com>
2018-02-20 16:18:15 -0500

    rerturn res

---
+++ seven.js
@@ -0,0 +1,12 @@
+// Write a function add that is
+// generalized for any amount of arguments
+
+function add(...num) {
+  res = 0;
+  for (i in num) {
+    res = +i;
+  }
+  return res;
+}
+
+console.log(add(1, 2, 3, 4, 5, 6));
