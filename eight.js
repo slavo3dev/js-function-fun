2db58d95464630affead2247d2abfb9d6cf50475
manjabogicevic <slavo@mimicom24.com>
2018-02-20 16:45:37 -0500

    adding i as a variable

--- eight.js
+++ eight.js
@@ -9,3 +9,15 @@ function sub(...nums) {
 }
 
 console.log(sub(1, 2, 4));
+
+console.log('***********************************');
+
+function sub_one(...nums) {
+  res = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    res -= nums[i];
+  }
+  return res;
+}
+
+console.log(sub_one(1, 2, 4));
