0d806d67158e80493b408a62712952454c512e83
manjabogicevic <slavo@mimicom24.com>
2018-02-20 16:52:00 -0500

    min function

--- nine.js
+++ nine.js
@@ -1,3 +1,11 @@
 // Write a function
 // min that is generalized
 // for any amount of arguments
+
+function min(...nums) {
+  return nums.reduce((total, num) => {
+    return total < num ? total : num;
+  });
+}
+
+console.log(min(1, 2, 4));
