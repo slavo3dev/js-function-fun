caa68f15dd9cbcdb78f3304cbefa3928ff31d3f0
manjabogicevic <slavo@mimicom24.com>
2018-02-21 19:57:15 -0500

    adding contect

--- nine.js
+++ nine.js
@@ -12,10 +12,10 @@ console.log(min(1, 2, 4));
 
 console.log('*********************');
 
-function min_one(...nums) {
-  return nums.reduce((result, num) => {
-    return minb(result, num);
-  }, Number.MAX_VALUE);
-}
+// function min_one(...nums) {
+//   return nums.reduce((result, num) => {
+//     return minb(result, num); using func from min
+//   }, Number.MAX_VALUE);
+// }
 
 console.log(min_one(1, 2, 4));
